b22df56087bd7afb533438d665ed0da2a997dcab
Richard Ni <rni@hmc.edu>
2023-09-17 21:38:36 +0000

    Reorganize connect_via_envvars

---
+++ src/connect.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from sqlalchemy import Engine, create_engine
+
+
+def connect_via_envvars(
+    env_file_path: Path = Path(__file__).parent.parent.parent / ".env",
+) -> Engine:
+    # Load environment variables from the .env file
+    load_dotenv(dotenv_path=env_file_path)
+
+    DATABASE_HOST = os.getenv("DATABASE_HOST")
+    DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    DATABASE = os.getenv("DATABASE")
+
+    connection_string = (
+        "mysql+mysqlconnector://"
+        f"{DATABASE_USERNAME}:{DATABASE_PASSWORD}"
+        f"@{DATABASE_HOST}:3306/{DATABASE}"
+    )
+
+    engine = create_engine(connection_string, echo=True)
+    return engine
--- src/db_migrations/database_init.py
+++ src/db_migrations/database_init.py
@@ -1,36 +1,8 @@
-import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-from sqlalchemy import Engine, create_engine, text
+from sqlalchemy import Engine, text
 
-
-def connect_via_envvars() -> Engine:
-    # Load environment variables from the .env file
-    load_dotenv(dotenv_path=Path(__file__).parent.parent.parent / ".env")
-
-    DATABASE_HOST = os.getenv("DATABASE_HOST")
-    DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-    DATABASE = os.getenv("DATABASE")
-
-    connection_string = (
-        "mysql+mysqlconnector://"
-        f"{DATABASE_USERNAME}:{DATABASE_PASSWORD}"
-        f"@{DATABASE_HOST}:3306/{DATABASE}"
-    )
-
-    engine = create_engine(connection_string, echo=True)
-    return engine
-
-
-def delete_table_by_name(engine: Engine, table_name: str):
-    with engine.connect() as connection:
-        connection.execute(text(f"DROP TABLE IF EXISTS {table_name}"))
-
-
-def delete_all_tables(engine: Engine):
-    delete_table_by_name(engine, "example")
+from src.connect import connect_via_envvars
 
 
 def execute_sql_file(engine: Engine, filepath: Path):
@@ -58,4 +30,4 @@ def delete_db(engine: Engine):
 if __name__ == "__main__":
     engine = connect_via_envvars()
     initialize_db(engine)
-    # delete_db(engine)
+    delete_db(engine)
