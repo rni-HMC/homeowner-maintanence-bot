c8b9bac361502fffa8e5a9d051fb09210673db40
Richard Ni <rni@hmc.edu>
2023-10-01 01:25:00 +0000

    Use envvar to point to .env filepath

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 from src.connect import connect_via_envvars
@@ -8,6 +10,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_countries():
-    engine = connect_via_envvars()
+    engine = connect_via_envvars(env_file_path=os.getenv("ENV_FILE_PATH"))
     tasks_to_display = get_tasks(engine)
     return render_template("home.html", tasks=tasks_to_display)
